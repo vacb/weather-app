dbd28c902ebc6e20d72846643cee16f1a3642b42
Victoria Blackburn <v.a.c.blackburn@gmail.com>
2021-03-24 12:55:22 +0000

    Get latitude and longitude from Navigator interface

---
+++ app.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+    let long;
+    let lat;
+
+    //only works if geolocation allowed
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+        })
+    } else {
+        h1.textContent = "Location required "
+    }
+});
