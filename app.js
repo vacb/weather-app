c61451ab14e60c92ab6b64c89febb9a61d7da407
Victoria Blackburn <v.a.c.blackburn@gmail.com>
2021-03-24 16:08:30 +0000

    Refactoring to extract icon mapping function

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 window.addEventListener('load', () => {
     let long;
     let lat;
-    let tempDescription = document.querySelector('.temp-description');
-    let tempDegree = document.querySelector('.temp-degree');
-    let locationName = document.querySelector('.location-name');
-    let temperature = document.querySelector('.temp');
+    const tempDescription = document.querySelector('.temp-description');
+    const tempDegree = document.querySelector('.temp-degree');
+    const locationName = document.querySelector('.location-name');
+    const temperature = document.querySelector('.temp');
     const tempSpan = document.querySelector('.degree-section span');
 
     // Only works if geolocation allowed
@@ -26,40 +26,16 @@ window.addEventListener('load', () => {
                     let {icon} = data.weather[0];
                     let temp = Math.floor(data.main.temp);
                     const name = data.name;
-                    console.log(name);
+                 
                     // Set DOM elements from API
                     tempDegree.textContent = temp;
                     tempDescription.textContent = description;
                     locationName.textContent = name;
 
                     let farenheit = Math.floor(temp * 1.8 + 32);
-                    // let celsius = (temp - 32) * 5 / 9;
-
-                    // Map the openweathermap icon codes per their API to the skycons icon ids
-                    const skyconMap = {
-                        z01d: 'clear-day',
-                        z02d: 'partly-cloudy-day',
-                        z03d: 'cloudy',
-                        z04d: 'cloudy',
-                        z09d: 'showers-day',
-                        z10d: 'rain',
-                        z11d: 'thunder',
-                        z13d: 'snow',
-                        z50d: 'fog',
-                        z01n: 'clear-night',
-                        z02n: 'partly-cloudy-night',
-                        z03n: 'cloudy',
-                        z04n: 'cloudy',
-                        z09n: 'showers-night',
-                        z10n: 'rain',
-                        z11n: 'thunder',
-                        z13n: 'snow',
-                        z50n: 'fog'
-                    }
-                 
-                    icon = 'z' + icon;
-                    icon = skyconMap[icon].toUpperCase();
-                    setIcons(icon, document.querySelector('.icon'));
+                   
+                    // Skycons set icons function
+                    setIcons(iconMap(icon), document.querySelector('.icon'));
 
                     //Change temperature to Celsius/Farenheit
                     temperature.addEventListener('click', () => {
@@ -77,7 +53,7 @@ window.addEventListener('load', () => {
         h1.textContent = "Location required for correct app function"
     }
 
-function setIcons(icon, iconID) {
+const setIcons= (icon, iconID) => {
     const skycons = new Skycons({ color: 'white' });
        skycons.play();
     return skycons.set(iconID, Skycons[icon]);
